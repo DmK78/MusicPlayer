a53af4e525de7c342cd839d55866ca1b005164f5
DmK78 <Ek193burg>
2019-03-04 15:18:33 +0500

    continue to make music player

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.PorterDuff;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toolbar;
 
@@ -16,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void magnify(View view) {
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.animate().scaleY(1.5f).scaleX(1.5f).rotation(360).setDuration(1000);
+        imageView.animate().scaleY(1).scaleX(1).rotation(360).setDuration(1000);
     }
 }
